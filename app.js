efd8ddfdfab0bb33d8c89f2467d0bf86aa6c8e4d
Antoine <antoine.ando@gmail.com>
2021-09-05 19:35:53 +0200

    first express and vue integration

--- app.js
+++ app.js
@@ -7,16 +7,62 @@ var cors = require('cors');
 var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 
+const passport = require('passport');
+const bodyParser = require('body-parser');
+const expressSession = require('express-session')({
+  secret: 'secret',
+  resave: false,
+  saveUninitialized: false
+});
+
 var app = express();
 
 app.use(logger('dev'));
 app.use(cors());
+/*
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+*/
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(expressSession);
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
+//app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+
+app.use(express.static('public'));
+app.use(express.static('dist'));
+
+/* Routing setup */
 app.use('/', indexRouter);
 //app.use('/users', usersRouter);
+
+
+/* MONGOOSE SETUP */
+const mongoose = require('mongoose');
+const passportLocalMongoose = require('passport-local-mongoose');
+
+mongoose.connect('mongodb+srv://memetailleUser:papillon44@cluster0.ofx2k.mongodb.net/memetaille?retryWrites=true&w=majority',
+  { useNewUrlParser: true, useUnifiedTopology: true });
+
+const Schema = mongoose.Schema;
+const UserSchema = new Schema({
+  username: String,
+  password: String
+});
+
+UserSchema.plugin(passportLocalMongoose);
+const UserDetails = mongoose.model('memetaille', UserSchema, 'users'); //Nom de la DB, Schema, nom de la collection
+
+/*
+UserDetails.register({username:'paul', active: false}, 'paul');
+UserDetails.register({username:'jay', active: false}, 'jay');
+UserDetails.register({username:'roy', active: false}, 'roy');
+*/
+
 
 module.exports = app;
--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,39 @@
 var express = require('express');
 var router = express.Router();
 
+/*
+router.get('/', passport.authenticate('basic', { session: false }), function(req, res, next) {
+
+	res.json({
+        message: 'OK'
+    });
+});
+*/
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.post('/login', function(req, res, next) {
+
+	passport.authenticate('local',
+	(err, user, info) => {
+		if (err) {
+			return next(err);
+		}
+
+		if (!user) {
+			return res.redirect('/test?info=' + info);
+		}
+
+		req.logIn(user, function(err) {
+			if (err) {
+				return next(err);
+			}
+
+			return res.redirect('/');
+		});
+
+	})(req, res, next);
+
+
     res.json({
         message: 'Test'
     });
