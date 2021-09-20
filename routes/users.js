b1d7efebb2c6dacb2e3f43515270aa9c5624fcb3
Antoine <antoine.ando@gmail.com>
2021-09-20 21:55:25 +0300

    User controller implemented

--- routes/users.js
+++ routes/users.js
@@ -2,40 +2,15 @@ var express = require('express');
 var router = express.Router();
 
 var UserModel = require('../db/models/users.js');
+var userController = require("../controllers/usersController.js")
 
-var connectToDatabase = require('../db/connection')
+//Requete GET pour lister tous les utilisateurs de même taille
+router.get('/', userController.index);
 
-/* GET users listing. */
-router.get('/', async function(req, res, next) {
+//Requete POST pour insérer un utilisateur
+router.post('/', userController.user_create_post)
 
-	const users = await UserModel.find({});
-
-		console.log(users)
-
-	try{
-		res.status(200).send(users)
-	}catch(error){
-		console.log(error)
-		res.status(500).send(error)
-	}
-});
-
-router.get("/test", async function(req, res, next){
-	const testUser = new UserModel({
-		name 	: "testUser",
-		active	: true,
-		height	: 180
-	})
-
-	try{
-		await testUser.save()
-		res.send(testUser)
-	}catch(error){
-		console.log(error)
-		res.status(500).send(error)
-	}
-
-	res.status(200)
-})
+//Requete GET pour récupérer un utilisateur par son username
+router.get('/:username', userController.user_get)
 
 module.exports = router;
